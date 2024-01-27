9f8639082fd6e3fd3dd734a0469d0b4f0d40555a
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-01-27 16:15:39 -0500

    footer updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center py-2 mb-5'>
-        <p className='text-lg'>Copyright &copy;2024 .All rights reserved. Made by Mukund</p>
+        <p className='text-lg'>Copyright &copy;2024.All rights reserved. Made by Mukund</p>
     </div>
   )
 }
