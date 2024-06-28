de61503007dde616ecea4e9f52c44ed8789befdb
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-06-28 11:20:17 -0400

    404 fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    rewrite: ({ path }) => {
+      if (!path.startsWith('/static/')) {
+        return '/index.html';
+      }
+      return null;
+    },
+  },
 })
